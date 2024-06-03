b39fa55b5eda5faa27f45c00c5cb047dbfd67ea5
Roushathbeeviks <roushisiddiq09@gmail.com>
2024-06-03 10:48:21 +0530

    todo

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -6,7 +6,7 @@ function TodoList({todos,status,deleteTodo}) {
     <div>
       {todos.map((todo) =>(
         <div className='list-input'>
-            <input value={todo.status} onClick={() => status(todo.id)}  type='checkbox'></input>
+            <input value={todo.status} onClick={() => status(todo.id)} checked={todo.status} type='checkbox'></input>
             <p>{todo.text}</p>
             <i class="fa fa-ban" onClick={() => deleteTodo(todo.id)} aria-hidden="true"></i>
         </div>
